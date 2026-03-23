e2d51d45b85241d5cb35ad1709bfb591c8efab50
bmmusngi <benedict.musngi@gmail.com>
2026-03-23 15:23:40 +0800

    bugfix: updated main.tsx and added App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DashboardLayout } from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardLayout />
+    </div>
+  );
+}
+
+export default App;
